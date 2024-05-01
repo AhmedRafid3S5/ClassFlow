318b092939d98a80112a935c38ce6f35633baf4c
Fa-riya <fariyaahmed219@gmail.com>
2024-05-01 23:12:23 +0600

    Admin Dashboard skeleton done

---
+++ Source folder for Admin dashboard/src/BookingReq.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+function BookingRequests() {
+  return (
+    <div className="pane">
+      <h2>Booking Requests</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Dept</th>
+            <th>ID</th>
+            <th>Booking Request</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>CSE</td>
+            <td>210041149</td>
+            <td>
+              Algorithm Class for 2nd year students (Extra Class for covering
+              syllabus)
+            </td>
+            <td >
+              <button>Approve</button>
+              <button>Decline</button>
+            </td>
+          </tr>
+          <tr>
+            <td>CSE</td>
+            <td>210041219</td>
+            <td>Room needed for CP class</td>
+            <td >
+              <button>Approve</button>
+              <button>Decline</button>
+            </td>
+          </tr>
+          <tr>
+            <td>EEE</td>
+            <td>210021304</td>
+            <td>Room Needed for Circuit Workshop</td>
+            <td>
+              <button>Approve</button>
+              <button>Decline</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default BookingRequests;
